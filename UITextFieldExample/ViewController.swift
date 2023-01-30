bbbfed80d56e0e603b98c836bd330590cfcd7d5d
enesgumus <gumus.enes1247@gmail.com>
2023-01-30 09:34:21 +0300

    Initial Commit

---
+++ UITextFieldExample/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  UITextFieldExample
+//
+//  Created by Enes Gümüş on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        errorLabel.isHidden = true
+
+    }
+
+    @IBAction func didTaploginButton( sender: UIButton)
+    {
+        if usernameTextField.text == "enesgumus",passwordTextField.text == "1234"
+        {
+            errorLabel.isHidden = false
+            activityIndicatorView.startAnimating()
+            errorLabel.text = "Giriş Başarılı"
+            
+            
+        }
+        
+        else
+        
+        {
+            if usernameTextField.text != "enesgumus", passwordTextField.text != "1234"
+            {
+                errorLabel.isHidden = false
+                errorLabel.text = "!Hatalı Kullanıcı adı veya Şifre"
+                activityIndicatorView.stopAnimating()
+            }
+        }
+            
+    }
+    
+}
+
+
